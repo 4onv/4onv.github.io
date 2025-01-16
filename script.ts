bfe708faad839309df9c7831cc5a24aeb4365bac
4onv <c0nv3rt10n@gmail.com>
2025-01-16 10:10:15 -0600

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,6 +1,15 @@
-// Sample TypeScript file (script.ts)
-function showMessage(message: string) {
-    console.log(message);
+interface Resource {
+    name: string;
+    url: string;
 }
 
-showMessage("Hello, this is TypeScript!");
+const resources: Resource[] = [
+    { name: "National Inquiry into Missing and Murdered Indigenous Women and Girls", url: "https://www.mmiwg-ffada.ca" },
+    { name: "Indian Residential Schools Crisis Line", url: "https://www.irsss.ca" },
+    { name: "Assembly of First Nations", url: "https://www.afn.ca" },
+    { name: "Indigenous Services Canada", url: "https://www.sac-isc.gc.ca" }
+];
+
+resources.forEach(resource => {
+    console.log(`${resource.name}: ${resource.url}`);
+});
